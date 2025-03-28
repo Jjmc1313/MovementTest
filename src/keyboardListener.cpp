52edbab991feb156650fb02a975cd7a38004ea45
J_jmc <johnlhartshorne@gmail.com>
2025-03-28 01:20:25 -0700

    added WASD support

--- src/keyboardListener.cpp
+++ src/keyboardListener.cpp
@@ -20,18 +20,22 @@ void KBListener::listen(Player& targetPlayer) {
     }
 
     switch (ch) {
+        case 119:
         case 72:
             if (targetPlayer.dirVector != DOWN) targetPlayer.dirVector = UP;
             break;
     
+        case 115:
         case 80:
             if (targetPlayer.dirVector != UP) targetPlayer.dirVector = DOWN;
             break;
     
+        case 97:
         case 75:
             if (targetPlayer.dirVector != RIGHT) targetPlayer.dirVector = LEFT;
             break;
     
+        case 100:
         case 77:
             if (targetPlayer.dirVector != LEFT) targetPlayer.dirVector = RIGHT;
             break;
